8191195778df6abdb4071d68d8319f94d9bd6b24
hendri <hendri.huai@gmail.com>
2016-07-18 10:42:24 +0700

    first commit

---
+++ app/src/main/java/com/ic/hendr1/travistestproject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ic.hendr1.travistestproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
